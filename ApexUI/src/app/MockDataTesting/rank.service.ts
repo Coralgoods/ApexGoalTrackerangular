c963fa303d41cb7897d40256d544f2301a335e32
EMatsonE <90651995+EMatsonE@users.noreply.github.com>
2022-04-07 19:56:10 -0500

    it works kinda
    html added

---
+++ ApexUI/src/app/MockDataTesting/rank.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { IRank } from '../interfaces/IRank';
+@Injectable({
+  providedIn: 'root'
+})
+export class RankService {
+
+  goals: IRank[] = 
+  [
+    {userName:"kevin", rankName:"bronze",rankScore:450},
+    {userName:"kevin", rankName:"silver",rankScore:1500}
+  ]
+  constructor() { }
+  listAllGoals()
+  {
+    return this.goals
+  }
+}
